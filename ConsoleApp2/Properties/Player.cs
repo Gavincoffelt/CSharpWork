9e748070f2af167ecfd2b09bc73f6c1a054246f4
Gavin Coffelt <gavincoffelt@yahoo.com>
2019-02-01 17:15:59 -0800

    CSharpPractice

---
+++ ConsoleApp2/Properties/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Properties
+{
+    class Player
+    {
+        private string name;
+        public int kills;
+        public int deaths;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return kills - deaths;
+            }
+        }
+         
+
+    }
+
+}
---
+++ ConsoleApp2/Properties/Vec3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    struct Vec3
+    {
+        
+      public float x, y, z;
+        public Vec3 Sum(Vec3 vec)
+        {
+
+            x = x + vec.x;
+            y = y + vec.y;
+            z = z + vec.z;
+
+            return vec;
+
+        }
+        public Vec3 Difference(Vec3 vec)
+        {
+
+            x = x - vec.x;
+            y = y - vec.y;
+            z = z - vec.z;
+
+            return vec;
+
+        }
+        public float Mag
+        {
+            get
+            {
+                return x * x + y * y + z * z;
+            }
+
+        }
+        public Vec3 Normalize
+        {
+            get
+            {
+                float mag = Mag;
+                Vec3 normal;
+                normal.x = x / mag;
+                normal.y = y / mag;
+                normal.z = z / mag;
+                return normal;
+            }
+            
+        }
+        public float Dot(Vec3 vec)
+        {
+            return (x * vec.x) + (y + vec.y) + (z * vec.z);
+        }
+
+    }
+}
